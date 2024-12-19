6b5086b12822e384fdfd36d455569a2744a092d1
elizabeth <elizabeth.pincheira.caro@gmail.com>
2024-12-19 19:33:19 -0300

    ngIf en home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -2,17 +2,21 @@ import { Component } from '@angular/core';
 import { IonHeader, IonContent, IonList, IonItem, IonSelectOption, IonSelect } from '@ionic/angular/standalone';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { CirculoPage } from '../circulo/circulo.component';
+import { TrianguloPage } from '../triangulo/triangulo.page'
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonContent, IonList, IonItem, IonSelectOption, IonSelect, FormsModule],
+  imports: [IonHeader, IonContent, IonList, IonItem, IonSelectOption, IonSelect, FormsModule, CommonModule, CirculoPage, TrianguloPage],
 })
 export class HomePage {
 
   figuraSeleccionada = "";
-
   constructor(private router: Router){}
 
 
@@ -20,10 +24,7 @@ export class HomePage {
   onFigura(){
     console.log(this.figuraSeleccionada)
 
-    if(this.figuraSeleccionada === "triangulo")
-      this.router.navigate(['/triangulo'])
-    else
-      this.router.navigate(['/circulo'])
+   
 
   }
 }
